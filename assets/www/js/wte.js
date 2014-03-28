d9bf58f9720d118be685e1b5800324d76211c75c
Wei Wang <wwei1987@gmail.com>
2014-03-28 15:27:59 -0700

    add query mobile
    not complete

--- assets/www/js/wte.js
+++ assets/www/js/wte.js
@@ -1,103 +1,103 @@
-$(document).ready(function() {
+	$(document).ready(function() {
 
-	var running = 0, 
-	timer, 
-	popup = $("#popbox-wrapper"), 
-	list = $("#list"), 
-	h1 = $("#big"), 
-	h1b4 = $("span", h1).eq(0), 
-	h1a5 = $("span", h1).eq(-1), 
-	what = $("#what"), times = 0;
-	
-	var menuList = [
-			["breakfast", "豆浆油条, 小笼包, 煎饼, 鸡蛋灌饼, 粥, 馄饨, 麦当劳"],
-			["lunch", "Hamburger, Sandwich, Panini, Pizza, Only Fruits, Salads"], 
-			["dinner", "KFC, Subway, Popeye, Papa john's, Korean Town, Chinatown, Little Tokyo"]
+		var running = 0, 
+		timer, 
+		popup = $("#popbox-wrapper"), 
+		list = $("#list"), 
+		h1 = $("#big"), 
+		h1b4 = $("span", h1).eq(0), 
+		h1a5 = $("span", h1).eq(-1), 
+		what = $("#what"), times = 0;
+		
+		var menuList = [
+		["breakfast", "豆浆油条, 小笼包, 煎饼, 鸡蛋灌饼, 粥, 馄饨, 麦当劳"],
+		["lunch", "Hamburger, Sandwich, Panini, Pizza, Only Fruits, Salads"], 
+		["dinner", "KFC, Subway, Popeye, Papa john's, Korean Town, Chinatown, Little Tokyo"]
 		];
 
-	var objDate = new Date();
-	var hours = objDate.getHours();
-	if(hours >= 0 && hours < 10) {
-	} else if(hours >= 10 && hours <= 14) {
-		menuList.push(menuList.shift());
-		list.val(menuList[0][1]);
-		h1b4.html(" is my <br />" + menuList[0][0]);
-	} else {
-		menuList.push(menuList.shift());
-		menuList.push(menuList.shift());
-		list.val(menuList[0][1]);
-		h1b4.html(" is my <br />" + menuList[0][0]);
-	}
-
-	$("#start").click(function() {
-		var l = list.val().split(",");
-		if(l.length == 1 && l[0] != "")
-			return alert("Really? There is only one choice!");
-		if(l.length == 1)
-			return alert("Are you serious? No choices in the menu at all!");
-		if(!running) {
-			times++;
-			if(times == 3) {
-				$("<p class='tip'><i></i>No favorite food? Remember you can edit the menu!</p>").css({
-					left:"58%",
-					top:"80%",
-					opacity : 0
-				}).appendTo("#mian_box").animate({
-					left : "-=5px",
-					opacity : 1
-				});
-				$("#start").val("Continue");
-				$("#edit_menu").add("#start").one("click", function() {
-					$(".tip").animate({
-						left : "+=5px",
-						opacity : 0
-					}, function() {
-						$(this).remove();
-					});
-				});
-				return false;
-			}
-
-			h1a5.text("?");
-			$(this).val("Stop");
-			timer = setInterval(function() {
-				var r = Math.floor(Math.random() * l.length) + 1;
-				var food = l[r - 1];
-				var h = $("body").height();
-				var w = $("body").width();
-				var rTop = Math.floor(Math.random() * h) + 1;
-				var rLeft = Math.floor(Math.random() * w) + 1;
-				//var rSize = Math.floor(Math.random() * (37 - 14 + 1)) + 14;
-				var emSize = Math.floor((Math.random()*1.5)+1);
-				what.html(food);
-				$("<span class='temp'>" + food + "</span>").css({
-					"display" : "none",
-					"position" : "absolute",
-					"top" : rTop,
-					"left" : rLeft,
-					"color" : "rgba(0,0,0," + Math.random() + ")",
-					"font-size" : emSize + "em"
-				}).appendTo("body").fadeIn("slow", function() {
-					$(this).fadeOut("slow", function() {
-						$(this).remove();
-					});
-				});
-			}, 60);
-			running = 1;
-
+		var objDate = new Date();
+		var hours = objDate.getHours();
+		if(hours >= 0 && hours < 10) {
+		} else if(hours >= 10 && hours <= 14) {
+			menuList.push(menuList.shift());
+			list.val(menuList[0][1]);
+			h1b4.html(" is my <br />" + menuList[0][0]);
 		} else {
-			h1a5.text("! ");
-			$("#start").val("No, try again!");
-			clearInterval(timer);
-			running = 0;
+			menuList.push(menuList.shift());
+			menuList.push(menuList.shift());
+			list.val(menuList[0][1]);
+			h1b4.html(" is my <br />" + menuList[0][0]);
 		}
-	});
+
+		$("#start").click(function() {
+			var l = list.val().split(",");
+			if(l.length == 1 && l[0] != "")
+				return alert("Really? There is only one choice!");
+			if(l.length == 1)
+				return alert("Are you serious? No choices in the menu at all!");
+			if(!running) {
+				times++;
+				if(times == 3) {
+					$("<p class='tip'><i></i>No favorite food? Remember you can edit the menu!</p>").css({
+						left:"58%",
+						top:"80%",
+						opacity : 0
+					}).appendTo("#mian_box").animate({
+						left : "-=5px",
+						opacity : 1
+					});
+					$("#start").text("Continue");
+					$("#edit_menu").add("#start").one("click", function() {
+						$(".tip").animate({
+							left : "+=5px",
+							opacity : 0
+						}, function() {
+							$(this).remove();
+						});
+					});
+					return false;
+				}
+
+				h1a5.text("?");
+				$("#start").text("Stop");
+				timer = setInterval(function() {
+					var r = Math.floor(Math.random() * l.length) + 1;
+					var food = l[r - 1];
+					var h = $("body").height();
+					var w = $("body").width();
+					var rTop = Math.floor(Math.random() * h) + 1;
+					var rLeft = Math.floor(Math.random() * w) + 1;
+					//var rSize = Math.floor(Math.random() * (37 - 14 + 1)) + 14;
+					var emSize = Math.floor((Math.random()*1.5)+1);
+					what.html(food);
+					$("<span class='temp'>" + food + "</span>").css({
+						"display" : "none",
+						"position" : "absolute",
+						"top" : rTop,
+						"left" : rLeft,
+						"color" : "rgba(0,0,0," + Math.random() + ")",
+						"font-size" : emSize + "em"
+					}).appendTo("body").fadeIn("slow", function() {
+						$(this).fadeOut("slow", function() {
+							$(this).remove();
+						});
+					});
+				}, 60);
+				running = 1;
+
+			} else {
+				h1a5.text("! ");
+				$("#start").text("No, try again!");
+				clearInterval(timer);
+				running = 0;
+			}
+		});
 
 	$("#edit_menu").click(function() {
 		if(running == 1) { 
-			alert("Lottery is running! Wait please..."); 
+			alert("Lottery is running! Stop it first..."); 
 		} else {
-		 	var t = list.val();
+			var t = list.val();
 			list.focus().val("").val(t);
 			centrePopup();
 			loadPopup();
@@ -120,7 +120,7 @@ $(document).ready(function() {
 				left : "-10px"
 			}, 50, function() {
 				what.text("What");
-				h1b4.text(" is my " + menuList[1][0]);
+				h1b4.html(" is my<br />" + menuList[1][0]);
 				h1a5.text("? ");
 				list.val(menuList[1][1]);
 				menuList.push(menuList.shift());
@@ -138,37 +138,37 @@ $(document).ready(function() {
 		};
 	});
 
-					function loadPopup() {
+	function loadPopup() {
 
-							$("#popupBackground").css({
-								"opacity" : "0.5"
-							});
-							$("#popupBackground").fadeIn("slow");
-							popup.fadeIn("slow");
-					}
+		$("#popupBackground").css({
+			"opacity" : "0.5"
+		});
+		$("#popupBackground").fadeIn("slow");
+		popup.fadeIn("slow");
+	}
 
-					function disablePopup() {
-							$("#popupBackground").fadeOut("slow");
-							popup.fadeOut("slow")
-					}
+	function disablePopup() {
+		$("#popupBackground").fadeOut("slow");
+		popup.fadeOut("slow")
+	}
 
-					function centrePopup() {
-						var windowWidth = document.documentElement.clientWidth;
-						var windowHeight = document.documentElement.clientHeight;
+	function centrePopup() {
+		var windowWidth = document.documentElement.clientWidth;
+		var windowHeight = document.documentElement.clientHeight;
 
-						var popupHeight = popup.height();
-						var popupWidth = popup.width();
+		var popupHeight = popup.height();
+		var popupWidth = popup.width();
 
-						popup.css({
-							"position" : "absolute",
-							"top" : windowHeight / 2 - popupHeight / 2,
-							"left" : windowWidth / 2 - popupWidth / 2
-						});
+		popup.css({
+			"position" : "absolute",
+			"top" : windowHeight / 2 - popupHeight / 2,
+			"left" : windowWidth / 2 - popupWidth / 2
+		});
 
-						$("#popupBackground").css({
-							"height" : windowHeight
-						});
-					}
+		$("#popupBackground").css({
+			"height" : windowHeight
+		});
+	}
 
 
 });
